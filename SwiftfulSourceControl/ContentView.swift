516f9763f16f6673bd5430aebd1e9a8ea065a51a
Manuel <manuel@manuelparcon.com>
2024-10-02 12:26:28 +0800

    [Feature] Updates to ContentView

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -36,12 +36,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "house.fill")
+            Image(systemName: "globe")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful Thinking!")
+            Text("some New Title!")
             
-            Button("Subscribe now") {
+            Button("Click me!") {
                 
             }
             
