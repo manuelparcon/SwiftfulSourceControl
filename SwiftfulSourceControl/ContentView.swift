24896c94d2258d8e3d03d92da9237998b093410e
Manuel <manuel@manuelparcon.com>
2024-10-02 11:11:20 +0800

    [Feature] Subscribe now add back

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful!")
             
-            Button("Subscribe") {
+            Button("Subscribe now!") {
                 
             }
             
