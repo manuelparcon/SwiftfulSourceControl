426119a039499e7f33ab83e1bd304dfe434ceb34
Manuel Parcon <9116591+manuelparcon@users.noreply.github.com>
2024-10-02 18:04:25 +0800

    Mp/add push notifications (#2)
    Added push notifs per XYZ
    
    ---------
    
    Co-authored-by: Manuel <manuel@manuelparcon.com>

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -47,9 +47,9 @@ struct ContentView: View {
                             Image(systemName: "globe")
                                 .font(.largeTitle)
                                 .foregroundStyle(.tint)
-                            Text("Swiftful!!!!!")
+                            Text("Swiftful Thinking!!")
                             
-                            Button("Subscribe now!") {
+                            Button("Subscribe!") {
                                 
                             }
                         }
