76153a16520fce1e9b5f3d7f6c658a7e15187db6
Manuel <manuel@manuelparcon.com>
2024-10-03 16:33:47 +0800

    [Feature] Add Profileview

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Manuel Parcon on 10/3/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Manuel!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
